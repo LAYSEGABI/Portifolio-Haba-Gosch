e30c66abeb59220e1efc8bb437a2e98612a312ba
LAYSEGABI <laysegabrielly13@gmail.com>
2023-06-05 10:20:05 -0300

    teste

---
+++ main.js
@@ -0,0 +1,11 @@
+let paralaxImagem = document.getElementById('img-terra')
+let paralaxTexto = document.getElementById('paralax-texto')
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    //paralaxImagem.style.left = value * 1 + 'px';
+    paralaxTexto.style.marginTop = value * 1 + 'px';
+
+})
+
